074cdd480edd26ed3252ba9a087207b668e96dea
Diez01 <93144575+Diez01@users.noreply.github.com>
2023-04-07 13:53:00 -0600

    actualización 1.0.2
    se modifico prácticamente todo el js para que se pudieran sumar los productos agregados a la bolsa y nos diera el total, se modifico el estilo de la bolsa y un poco del html para cambiar un poco los valores de cada producto
    posdata: el dia de hoy solo dios y yo sabemos lo que se realizo, tal vez mañana solo dios lo entienda porque yo no

--- Proyecto_Optimizado/index.js
+++ Proyecto_Optimizado/index.js
@@ -1,18 +1,39 @@
-// Obtener todos los botones de "Comprar"
-const botonesComprar = document.querySelectorAll('.incrementButton');
+// Obtener todos los botones de incremento
+const incrementButtons = document.querySelectorAll('.incrementButton');
 
-// Obtener el contador de productos
-const contadorProductos = document.querySelector('.counter');
-
-// Inicializar el valor del contador en 0
-let contador = 0;
-
-// Agregar un evento click a cada botón de "Comprar"
-botonesComprar.forEach((boton) => {
-  boton.addEventListener('click', () => {
-    // Incrementar el contador
-    contador++;
-    // Actualizar el valor del contador
-    contadorProductos.textContent = contador;
-  });
+// Agregar un evento de clic a cada botón de incremento
+incrementButtons.forEach(button => {
+  button.addEventListener('click', addToCart);
 });
+
+// Función para agregar un producto al carrito
+function addToCart(event) {
+  // Obtener el producto seleccionado
+  const producto = event.target.parentNode.parentNode;
+  
+  // Crear un nuevo elemento para el carrito
+  const cartItem = document.createElement('div');
+  cartItem.classList.add('cart-item');
+  
+  // Agregar el título del producto al elemento del carrito
+  const cartItemTitle = document.createElement('h3');
+  cartItemTitle.innerText = producto.querySelector('.titulo').innerText;
+  cartItem.appendChild(cartItemTitle);
+  
+  // Agregar el precio del producto al elemento del carrito
+  const cartItemPrice = document.createElement('h4');
+  cartItemPrice.innerText = producto.querySelector('hr').nextSibling.textContent.trim();
+  cartItem.appendChild(cartItemPrice);
+  
+  // Agregar el elemento del carrito a la lista de elementos
+  const cartItemsList = document.querySelector('.elementos');
+  cartItemsList.appendChild(cartItem);
+  
+  // Actualizar el contador de elementos en el carrito
+  const cartCounter = document.querySelector('.counter');
+  cartCounter.innerText = parseInt(cartCounter.innerText) + 1;
+  
+  // Actualizar el precio total
+  const precioTotal = document.querySelector('.precio-total');
+  precioTotal.innerText = parseInt(precioTotal.innerText) + parseInt(cartItemPrice.innerText);
+}
